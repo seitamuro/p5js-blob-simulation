79dc4b6414a1bb1b0a249860b229e65511cba697
seitamuro <seitamuro0210@gmail.com>
2023-05-21 05:04:24 +0900

    モジホコリカビを表すBlobクラスを作成

--- sketch.ts
+++ sketch.ts
@@ -1,40 +1,75 @@
 import p5 from 'p5'
 
+/*
+ * Blobクラス
+ * モジホコリカビ本体の動き
+ */
+class Blob {
+    x: number // x座標
+    y: number // y座標
+    vx: number // x方向の速度
+    vy: number // y方向の速度
+    radius: number // 半径
+
+    /*
+     * Blobクラスのコンストラクタ
+     *
+     * @param x x座標
+     * @param y y座標
+     *
+     * @return Blob
+     */
+    constructor(x: number, y: number) {
+        this.x = x
+        this.y = y
+        this.radius = 10
+        this.vx = 1
+        this.vy = 1
+    }
+
+    /*
+     * Blobを描画する
+     *
+     * @param p p5インスタンス
+     */
+    draw(p: p5) {
+        p.ellipse(this.x, this.y, this.radius, this.radius)
+    }
+
+    /*
+     * Blobを動かす
+     *
+     * @param p p5インスタンス
+     */
+    move(p: p5) {
+        if (this.x < 0 || this.x > p.width) {
+            this.vx *= -1
+        }
+        if (this.y < 0 || this.y > p.height) {
+            this.vy *= -1
+        }
+        this.x += this.vx
+        this.y += this.vy
+    }
+}
+
 const sketch = (p: p5) => {
-    let cx = 100
-    let cy = 100
-    let sx = 200
-    let sy = 200
-    let cvx = p.random(1, 2) * p.random([-1, 1])
-    let cvy = p.random(1, 2) * p.random([-1, 1])
-    let svx = p.random(1, 2) * p.random([-1, 1])
-    let svy = p.random(1, 2) * p.random([-1, 1])
+    const blobs: Blob[] = []
 
     p.setup = () => {
         p.createCanvas(400, 400)
+        let i: number
+        for (i = 0; i < 10; i++) {
+            blobs.push(new Blob(p.random(p.width), p.random(p.height)))
+        }
     }
 
     p.draw = () => {
-        p.background(220)
-        p.ellipse(cx, cy, 50, 50)
-        p.rect(sx, sy, 50, 50)
-
-        if (sx < 0 || sx > p.width) {
-            svx *= -1
-        }
-        if (sy < 0 || sy > p.height) {
-            svy *= -1
-        }
-        sx += svx
-        sy += svy
-        if (cx < 0 || cx > p.width) {
-            cvx *= -1
-        }
-        if (cy < 0 || cy > p.height) {
-            cvy *= -1
-        }
-        cx += cvx
-        cy += cvy
+        p.background(200)
+        blobs.forEach((blob) => {
+            blob.move(p)
+            blob.draw(p)
+        })
     }
 }
 
